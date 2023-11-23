8eba108cf075aaff25d254e55f0a6b0fbc092361
yoangga <21523235@students.uii.ac.id>
2023-11-23 21:04:17 +0700

    Commit

--- app/src/main/java/com/example/pharmalinkadmin/AddItemActivity.kt
+++ app/src/main/java/com/example/pharmalinkadmin/AddItemActivity.kt
@@ -1,12 +1,28 @@
 package com.example.pharmalinkadmin
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pharmalinkadmin.databinding.ActivityAddItemBinding
+import com.example.pharmalinkadmin.model.AllMenu
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 
 class AddItemActivity : AppCompatActivity() {
+
+    private lateinit var drugName: String
+    private lateinit var drugPrice: String
+    private lateinit var drugDescription: String
+    private var drugImageUri: Uri? = null
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+
     private val binding: ActivityAddItemBinding by lazy {
         ActivityAddItemBinding.inflate(layoutInflater)
     }
@@ -14,19 +30,77 @@ class AddItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+
+        binding.AddItemButton.setOnClickListener {
+            drugName = binding.EnterDrugName.text.toString().trim()
+            drugPrice = binding.EnterDrugPrice.text.toString().trim()
+            drugDescription = binding.description.text.toString().trim()
+
+            if (!(drugName.isBlank() || drugPrice.isBlank() || drugDescription.isBlank())) {
+                uploadData()
+                Toast.makeText(this, "Item Add Successfully", Toast.LENGTH_SHORT).show()
+                finish()
+            } else {
+                Toast.makeText(this, "Fill all the details", Toast.LENGTH_SHORT).show()
+            }
+        }
         binding.selectImage.setOnClickListener {
-            pickimage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+            pickImage.launch("image/*")
         }
 
-        binding.backButton.setOnClickListener{
+        binding.backButton.setOnClickListener {
             finish()
         }
     }
 
-    val pickimage = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-        if (uri != null) {
-            binding.selectedImage.setImageURI(uri)
+    private fun uploadData() {
+        val menuRef = database.getReference("menu")
+        val newItemKey = menuRef.push().key
+
+        if (drugImageUri != null) {
+
+            val storageRef = FirebaseStorage.getInstance().reference
+            val imageRef = storageRef.child("menu_images/${newItemKey}.jpg")
+            val uploadTask = imageRef.putFile(drugImageUri!!)
+
+            uploadTask.addOnSuccessListener {
+                imageRef.downloadUrl.addOnSuccessListener { downloadUrl ->
+                    val newItem = AllMenu(
+                        drugName = drugName,
+                        drugPrice = drugPrice,
+                        drugDescription = drugDescription,
+                        drugImage = downloadUrl.toString(),
+                    )
+
+                    newItemKey?.let { key ->
+                        menuRef.child(key).setValue(newItem).addOnSuccessListener {
+                            Toast.makeText(this, "data uploaded successfully", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                            .addOnFailureListener {
+                                Toast.makeText(this, "data uploaded failed", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                    }
+
+                }
+            }.addOnFailureListener {
+                Toast.makeText(this, "Image Upload failed", Toast.LENGTH_SHORT).show()
+            }
+
+        } else {
+            Toast.makeText(this, "Please select an image", Toast.LENGTH_SHORT).show()
         }
+
     }
 
+    private val pickImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        if (uri != null) {
+            binding.selectedImage.setImageURI(uri)
+            drugImageUri = uri
+        }
+    }
 }
---
+++ app/src/main/java/com/example/pharmalinkadmin/model/AllMenu.kt
@@ -0,0 +1,8 @@
+package com.example.pharmalinkadmin.model
+
+data class AllMenu(
+    val drugName: String? = null,
+    val drugPrice: String? = null,
+    val drugDescription: String? = null,
+    val drugImage: String? = null,
+)
