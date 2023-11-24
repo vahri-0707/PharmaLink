4b14c7e6a7c61ceb738f7ace713e0740a5624bd0
yoangga <21523235@students.uii.ac.id>
2023-11-24 19:07:59 +0700

    commit

--- app/src/main/java/com/example/pharmalink/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/pharmalink/Fragment/HomeFragment.kt
@@ -15,17 +15,25 @@ import com.denzcoskun.imageslider.models.SlideModel
 import com.example.pharmalink.MenuBootomSheetFragment
 import com.example.pharmalink.Notification_Bottom_Fragmet
 import com.example.pharmalink.R
+import com.example.pharmalink.adapter.MenuAdapter
 import com.example.pharmalink.adapter.PopularAdapter
 import com.example.pharmalink.databinding.FragmentHomeBinding
+import com.example.pharmalink.model.MenuItem
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var database: FirebaseDatabase
+    private lateinit var menuItems: MutableList<MenuItem>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -34,16 +42,58 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(layoutInflater)
-        binding = FragmentHomeBinding.inflate(inflater,container,false)
-        binding.notificationButton.setOnClickListener{
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.notificationButton.setOnClickListener {
             val bottomSheetDialog = Notification_Bottom_Fragmet()
-            bottomSheetDialog.show(parentFragmentManager,"Test")
+            bottomSheetDialog.show(parentFragmentManager, "Test")
         }
-        binding.viewAllBarang.setOnClickListener{
+        binding.viewAllBarang.setOnClickListener {
             val bottomSheetDialog = MenuBootomSheetFragment()
-            bottomSheetDialog.show(parentFragmentManager,"Test")
+            bottomSheetDialog.show(parentFragmentManager, "Test")
         }
+
+        retrieveAndDisplayPopularItems()
         return binding.root
+    }
+
+    private fun retrieveAndDisplayPopularItems() {
+        database = FirebaseDatabase.getInstance()
+        val drugRef: DatabaseReference = database.reference.child("menu")
+        menuItems = mutableListOf()
+
+        drugRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (drugSnapshot in snapshot.children) {
+                    val menuItem = drugSnapshot.getValue(MenuItem::class.java)
+                    menuItem?.let { menuItems.add(it) }
+                }
+                randomPopularItems()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    private fun randomPopularItems() {
+        val index = menuItems.indices.toList().shuffled()
+        val numItemToShow = 6
+        val subsetMenuItems = index.take(numItemToShow).map { menuItems[it] }
+
+        setPopularItemsAdapter(subsetMenuItems)
+    }
+
+    private fun setPopularItemsAdapter(subsetMenuItems: List<MenuItem>) {
+        val adapter = MenuAdapter(subsetMenuItems, requireContext())
+
+        // Use a GridLayoutManager with 2 columns
+        val layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.PopularRecyclerView.layoutManager = layoutManager
+
+        binding.PopularRecyclerView.adapter = adapter
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -57,7 +107,7 @@ class HomeFragment : Fragment() {
         imageSlider.setImageList(imageList)
         imageSlider.setImageList(imageList, ScaleTypes.FIT)
 
-        imageSlider.setItemClickListener(object :ItemClickListener {
+        imageSlider.setItemClickListener(object : ItemClickListener {
             override fun doubleClick(position: Int) {
                 TODO("Not yet implemented")
             }
@@ -68,22 +118,7 @@ class HomeFragment : Fragment() {
                 Toast.makeText(requireContext(), itemMessage, Toast.LENGTH_SHORT).show()
             }
         })
-        val namaBarang = listOf("Paracetamol","Procold","Bodrex","Amoxcilin")
-        val Price = listOf("Rp. 5000", "Rp. 7000", "Rp. 5000", "Rp. 15000")
-        val fotoBarangTerlarisImages = listOf(R.drawable.medicine_paracetamol, R.drawable.medicine_procold, R.drawable.medicine_bodrex, R.drawable.medicine_amoxiline)
 
-        val adapter = PopularAdapter(namaBarang, Price, fotoBarangTerlarisImages, requireContext())
-
-
-        // Use a GridLayoutManager with 2 columns
-        val layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-        binding.PopularRecyclerView.layoutManager = layoutManager
-
-        binding.PopularRecyclerView.adapter = adapter
-
-
-        }
-
-    companion object {
     }
+
 }
