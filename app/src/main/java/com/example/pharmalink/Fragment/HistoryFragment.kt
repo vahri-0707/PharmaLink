9e91a52464a7b4a4e99f2ebd2b2d17bb3112190a
yoangga <21523235@students.uii.ac.id>
2023-11-25 17:54:11 +0700

    commit

--- app/src/main/java/com/example/pharmalink/Fragment/HistoryFragment.kt
+++ app/src/main/java/com/example/pharmalink/Fragment/HistoryFragment.kt
@@ -1,26 +1,36 @@
 package com.example.pharmalink.Fragment
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.example.pharmalink.MainActivity
 import com.example.pharmalink.PayOutActivity
 import com.example.pharmalink.R
 import com.example.pharmalink.adapter.BuyAgainAdapter
 import com.example.pharmalink.databinding.FragmentHistoryBinding
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.pharmalink.model.OrderDetails
+import com.example.pharmalink.recentOrderItems
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class HistoryFragment : Fragment() {
     private lateinit var binding: FragmentHistoryBinding
     private lateinit var buyAgainAdapter: BuyAgainAdapter
+    private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
+    private lateinit var userId: String
+    private var listOfOrderItem: MutableList<OrderDetails> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -30,11 +40,19 @@ class HistoryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding=FragmentHistoryBinding.inflate(layoutInflater,container,false)
+        binding = FragmentHistoryBinding.inflate(layoutInflater, container, false)
         // Inflate the layout for this fragment
-        setupRecyclerView()
 
-        binding.buttonBackToHomee.setOnClickListener{
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+
+        retrieveBuyHistory()
+
+        binding.recentbuyitem.setOnClickListener{
+            seeItemsRecentBuy()
+        }
+
+        binding.buttonBackToHomee.setOnClickListener {
             val intent = Intent(requireContext(), MainActivity::class.java)
             startActivity(intent)
         }
@@ -42,28 +60,89 @@ class HistoryFragment : Fragment() {
         return binding.root
     }
 
-    private fun setupRecyclerView(){
-        val buyAgainDrugName = listOf("Paracetamol","Procold","Bodrex","Amoxcilin")
-        val buyAgainDrugPrice = listOf("Rp. 5000", "Rp. 7000", "Rp. 5000", "Rp. 15000")
-        val buyAgainDrugImage = listOf(
-            R.drawable.medicine_paracetamol,
-            R.drawable.medicine_procold,
-            R.drawable.medicine_bodrex,
-            R.drawable.medicine_amoxiline)
-        buyAgainAdapter = BuyAgainAdapter(buyAgainDrugName,buyAgainDrugPrice,buyAgainDrugImage)
-        binding.BuyAgainRecyclerView.adapter = buyAgainAdapter
-        binding.BuyAgainRecyclerView.layoutManager = LinearLayoutManager(requireContext())
-
+    private fun seeItemsRecentBuy() {
+        listOfOrderItem.firstOrNull()?.let { recentBuy ->
+            val intent = Intent(requireContext(), recentOrderItems::class.java)
+            intent.putExtra("RecentBuyOrderItem", recentBuy)
+            startActivity(intent)
+        }
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HistoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun retrieveBuyHistory() {
+        binding.recentbuyitem.visibility = View.INVISIBLE
+        userId = auth.currentUser?.uid ?: ""
+
+        val buyItemReference: DatabaseReference =
+            database.reference.child("user").child(userId).child("BuyHistory")
+        val shortingQuery = buyItemReference.orderByChild("currentTime")
+
+        shortingQuery.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (buySnapshot in snapshot.children) {
+                    val buyHistoryItem = buySnapshot.getValue(OrderDetails::class.java)
+                    buyHistoryItem?.let {
+                        listOfOrderItem.add(it)
+                    }
+                }
+                listOfOrderItem.reverse()
+                if (listOfOrderItem.isNotEmpty()) {
+                    setDataInRecentBuyItem()
+                    setPreviousBuyItemRecyclerView()
                 }
             }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
+
+    private fun setDataInRecentBuyItem() {
+        binding.recentbuyitem.visibility = View.VISIBLE
+        val recentOrderItem = listOfOrderItem.firstOrNull()
+        recentOrderItem?.let {
+            with(binding) {
+                buyAgainDrugName.text = it.drugNames?.firstOrNull() ?: ""
+                buyAgainDrugPrice.text = it.drugPrices?.firstOrNull() ?: ""
+                val image = it.drugImages?.firstOrNull() ?: ""
+                val uri = Uri.parse(image)
+                Glide.with(requireContext()).load(uri).into(buyAgainDrugImage)
+
+                listOfOrderItem.reverse()
+                if (listOfOrderItem.isNotEmpty()) {
+
+                }
+            }
+        }
+
+    }
+
+    private fun setPreviousBuyItemRecyclerView() {
+        val buyAgainDrugName = mutableListOf<String>()
+        val buyAgainDrugPrice = mutableListOf<String>()
+        val buyAgainDrugImage = mutableListOf<String>()
+
+        for (i in 1 until listOfOrderItem.size) {
+            listOfOrderItem[i].drugNames?.firstOrNull()?.let {
+                buyAgainDrugName.add(it)
+                listOfOrderItem[i].drugPrices?.firstOrNull()?.let {
+                    buyAgainDrugPrice.add(it)
+                    listOfOrderItem[i].drugImages?.firstOrNull()?.let {
+                        buyAgainDrugImage.add(it)
+                    }
+                }
+                val rv = binding.BuyAgainRecyclerView
+                rv.layoutManager = LinearLayoutManager(requireContext())
+                buyAgainAdapter = BuyAgainAdapter(
+                    buyAgainDrugName,
+                    buyAgainDrugPrice,
+                    buyAgainDrugImage,
+                    requireContext()
+                )
+                rv.adapter = buyAgainAdapter
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/pharmalink/adapter/BuyAgainAdapter.kt
+++ app/src/main/java/com/example/pharmalink/adapter/BuyAgainAdapter.kt
@@ -1,30 +1,45 @@
 package com.example.pharmalink.adapter
 
+import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.pharmalink.databinding.BuyAgainItemBinding
 
-class BuyAgainAdapter(private val buyAgainDrugName: List<String>, private val
-buyAgainDrugPrice: List<String>, private val buyAgainDrugImage: List<Int>
+class BuyAgainAdapter(
+    private val buyAgainDrugName: MutableList<String>,
+    private val buyAgainDrugPrice: MutableList<String>,
+    private val buyAgainDrugImage: MutableList<String>,
+    private var requireContext: Context
 ) : RecyclerView
-    .Adapter<BuyAgainAdapter.BuyAgainViewHolder>() {
+.Adapter<BuyAgainAdapter.BuyAgainViewHolder>() {
 
     override fun onBindViewHolder(holder: BuyAgainViewHolder, position: Int) {
-holder.bind(buyAgainDrugName[position],buyAgainDrugPrice[position],buyAgainDrugImage[position])
+        holder.bind(
+            buyAgainDrugName[position],
+            buyAgainDrugPrice[position],
+            buyAgainDrugImage[position]
+        )
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BuyAgainViewHolder {
-        val binding = BuyAgainItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding =
+            BuyAgainItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return BuyAgainViewHolder(binding)
     }
 
     override fun getItemCount(): Int = buyAgainDrugName.size
-    class BuyAgainViewHolder(private val binding: BuyAgainItemBinding): RecyclerView.ViewHolder
-        (binding.root){
-        fun bind(drugName: String, drugPrice: String, drugImage: Int) {
-            binding.buyAgainDrugName.text=drugName
-            binding.buyAgainDrugPrice.text=drugPrice
-            binding.buyAgainDrugImage.setImageResource(drugImage)
+    inner class BuyAgainViewHolder(private val binding: BuyAgainItemBinding) : RecyclerView.ViewHolder
+        (binding.root) {
+        fun bind(drugName: String, drugPrice: String, drugImage: String) {
+            binding.buyAgainDrugName.text = drugName
+            binding.buyAgainDrugPrice.text = drugPrice
+
+            val uriString = drugImage
+            val uri = Uri.parse(uriString)
+            Glide.with(requireContext).load(uri).into(binding.buyAgainDrugImage)
         }
 
     }
