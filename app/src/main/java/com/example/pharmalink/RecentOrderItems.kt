6f80dca60a5c6700901fc9a11e10328a401ebc86
Vahri Maulana <vhrimlna@gmail.com>
2023-11-26 00:53:57 +0700

    Vahri

--- app/src/main/java/com/example/pharmalink/RecentOrderItems.kt
+++ app/src/main/java/com/example/pharmalink/RecentOrderItems.kt
@@ -1,8 +1,11 @@
 package com.example.pharmalink
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.pharmalink.Fragment.CartFragment
+import com.example.pharmalink.Fragment.HistoryFragment
 import com.example.pharmalink.adapter.RecentBuyAdapter
 import com.example.pharmalink.databinding.ActivityRecentOrderItemsBinding
 import com.example.pharmalink.model.OrderDetails
@@ -17,31 +20,40 @@ class RecentOrderItems : AppCompatActivity() {
     private lateinit var allDrugImages: ArrayList<String>
     private lateinit var allDrugPrices: ArrayList<String>
     private lateinit var allDrugQuantities: ArrayList<Int>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val recentOrderItems =
+            intent.getSerializableExtra("RecentBuyOrderItem") as ArrayList<OrderDetails>?
+        recentOrderItems?.let { orderDetailsList ->
+            if (orderDetailsList.isNotEmpty()) {
+                val mostRecentOrderDetails = orderDetailsList.last()
 
-        val recentOrderItems = intent.getSerializableExtra("RecentBuyOrderItem") as ArrayList<OrderDetails>?
-        recentOrderItems?.let { orderDetails ->
-            if (orderDetails.isNotEmpty()) {
-                val recentOrderItem = orderDetails[0]
-
-                allDrugNames = recentOrderItem.drugNames as ArrayList<String>
-                allDrugImages = recentOrderItem.drugImages as ArrayList<String>
-                allDrugPrices = recentOrderItem.drugPrices as ArrayList<String>
-                allDrugQuantities = recentOrderItem.drugQuantities as ArrayList<Int>
+                allDrugNames = mostRecentOrderDetails.drugNames as ArrayList<String>
+                allDrugImages = mostRecentOrderDetails.drugImages as ArrayList<String>
+                allDrugPrices = mostRecentOrderDetails.drugPrices as ArrayList<String>
+                allDrugQuantities = mostRecentOrderDetails.drugQuantities as ArrayList<Int>
             }
         }
 
         setAdapter()
+
+        binding.buttonBackToHistory.setOnClickListener {
+            // Navigate to FragmentCart
+            val intent = Intent(this, CartFragment::class.java)
+            startActivity(intent)
+            finish() // Optional: Finish the current activity if you don't want to go back to it
+        }
     }
 
     private fun setAdapter() {
         val rv = binding.recyclerViewRecentBuy
         rv.layoutManager = LinearLayoutManager(this)
-        val adapter = RecentBuyAdapter(this,allDrugNames,allDrugImages,allDrugPrices,allDrugQuantities)
+        val adapter =
+            RecentBuyAdapter(this, allDrugNames, allDrugImages, allDrugPrices, allDrugQuantities)
         rv.adapter = adapter
     }
-
 }
+
